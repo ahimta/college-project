881442259a7ded3814b08c1a0707680c4e202e59
Abdullah Alansari <ahimta@gmail.com>
2014-05-19 16:00:35 +0300

    added: decidable shared spec

---
+++ spec/support/shared_specs/controllers/decidable.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+shared_examples 'controllers/decidable' do |model, entity, resource|
+  
+  name = resource[0..-2]
+  url = "/api/v1/#{resource}"
+
+  {'accept' => true, 'reject' => false}.each do |decision, accepted|
+    describe "POST #{url}/:id/#{decision}" do
+      let!(:rejected_record) { FactoryGirl.create(name, accepted: false) }
+      let!(:accepted_record) { FactoryGirl.create(name, accepted: true) }
+      let!(:pending_record) { FactoryGirl.create(name) }
+      let(:count) { 3 }
+
+      let(:action) { post "#{url}/#{record.id}/#{decision}" }
+
+      before { expect(model.count).to eq(count) }
+      after { expect(model.count).to eq(count) }
+
+      context 'exist' do
+        after { expect(record.reload.accepted).to be(accepted) }
+
+        context 'rejected' do
+          let(:record) { rejected_record }
+
+          before { expect(record.reload.accepted).to be(false) }
+
+          it { action }
+        end
+        context 'accepted' do
+          let(:record) { accepted_record }
+
+          before { expect(record.reload.accepted).to be(true) }
+
+          it { action }
+        end
+        context 'pending' do
+          let(:record) { pending_record }
+
+          before { expect(record.reload.accepted).to be(nil) }
+
+          it { action }
+        end
+      end
+      context 'does not exist' do
+        it { post "#{url}/99/#{decision}" }
+
+        after { expect(response.status).to eq(404) }
+      end
+    end
+  end
+end
