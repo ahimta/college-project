d33144809748a8624987281825b99cf052b154ed
Abdullah Alansari <ahimta@gmail.com>
2014-06-03 18:25:58 +0300

    Add full_name to Applicant entity

--- app/api/api/v1/entities/applicant.rb
+++ app/api/api/v1/entities/applicant.rb
@@ -3,6 +3,10 @@ class API::V1::Entities::Applicant < API::V1::Entities::Base
 
   expose :first_name, :last_name, :phone, :address, :specialization, :degree
 
+  expose :full_name do |applicant, _|
+    "#{applicant.first_name} #{applicant.last_name}"
+  end
+
   expose :accepted do |applicant, _|
     accepted = applicant.accepted
 
