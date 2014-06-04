42106df2795cc522d1ad0f7b1709a54ad06295b6
Abdullah Alansari <ahimta@gmail.com>
2014-06-04 18:18:35 +0300

    remove login spec from admins_spec

--- spec/api/api/v1/admins_spec.rb
+++ spec/api/api/v1/admins_spec.rb
@@ -21,55 +21,7 @@ describe API::V1::Admins do
     valid: (create_factories[:valid] + [:admin_without_password])
   }
 
-  resource = 'admins'
-  url = "/api/v1/#{resource}/login"
-  name = resource[0...-1]
-  model = Admin
-
-  describe "POST #{url}" do
-    let(:action!) { post url, params }
-    let!(:user) { FactoryGirl.create name }
-    let(:count)  { 1 }
-
-    before { expect(model.count).to eq(count) }
-    after { expect(model.count).to eq(count) }
-
-    context 'valid' do
-      let(:params) {
-        {login: {username: user.username, password: user.password} }
-      }
-
-      it { action! }
-
-      after { expect(session[:user_type]).to eq('admin') }
-      after { expect(session[:user_id]).to eq(user.id) }
-      after { expect(response.status).to eq(201) }
-    end
-    context 'invalid' do
-      after { expect(session[:user_type]).to be(nil) }
-      after { expect(session[:user_id]).to be(nil) }
-
-      context 'wrong password' do
-        let(:params) {
-          {login: {username: user.username, password: user.password.swapcase} }
-        }
-
-        it { action! }
-
-        after { expect(response.status).to eq(401) }
-      end
-
-      context 'invalid params' do
-        after { expect(response.status).to eq(400) }
-
-        context 'no user param' do
-          let(:params) { {} }
-
-          it { action! }
-        end
-      end
-    end
-  end
+  # it_behaves_like 'controllers/login', Admin, 'admins'
 
   context 'logged in' do
     it_behaves_like 'controllers/index', *args
