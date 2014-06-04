f2bb9b8d05358892a500869fd1b4d914b0335295
Abdullah Alansari <ahimta@gmail.com>
2014-06-04 18:19:10 +0300

    add controllers/login shared_spec

---
+++ spec/support/shared_specs/controllers/login.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+shared_examples 'controllers/login' do |model, resource|
+  url = "/api/v1/#{resource}/login"
+  name = resource[0...-1]
+
+  describe "POST #{url}" do
+    let(:action!) { post url, params }
+    let!(:user) { FactoryGirl.create name }
+    let(:count)  { 1 }
+
+    before { expect(model.count).to eq(count) }
+    after { expect(model.count).to eq(count) }
+
+    context 'valid' do
+      let(:params) {
+        {login: {username: user.username, password: user.password} }
+      }
+
+      it { action! }
+
+      after { expect(session[:user_type]).to eq('admin') }
+      after { expect(session[:user_id]).to eq(user.id) }
+      after { expect(response.status).to eq(201) }
+    end
+    context 'invalid' do
+      after { expect(session[:user_type]).to be(nil) }
+      after { expect(session[:user_id]).to be(nil) }
+
+      context 'wrong password' do
+        let(:params) {
+          {login: {username: user.username, password: user.password.swapcase} }
+        }
+
+        it { action! }
+
+        after { expect(response.status).to eq(401) }
+      end
+
+      context 'invalid params' do
+        after { expect(response.status).to eq(400) }
+
+        context 'no user param' do
+          let(:params) { {} }
+
+          it { action! }
+        end
+      end
+    end
+  end
+end
