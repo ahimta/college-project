ba559579b84effdcb601db18937f6f4ab1696aa6
Abdullah Alansari <ahimta@gmail.com>
2014-06-04 16:33:59 +0300

    add a dedicated Helpers module

---
+++ app/api/api/v1/helpers.rb
@@ -0,0 +1,11 @@
+module API::V1::Helpers
+  extend Grape::API::Helpers
+
+  def safe_params
+    @safe_params ||= declared(params, include_missing: false)
+  end
+
+  def session
+    @session ||= env[Rack::Session::Abstract::ENV_SESSION_KEY]
+  end
+end
