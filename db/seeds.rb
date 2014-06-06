f3ad61be3efcc65346a259e08132d6fe97cfb3db
Abdullah Alansari <ahimta@gmail.com>
2014-06-06 06:36:37 +0300

    adapt seeds.rb to new changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Applicant::JobRequest.destroy_all
+Admin::Account.destroy_all
 
-applicants = [
-  {first_name: 'الخليل', last_name: 'أحمد الفراهيدي', phone: '0512345678', address: 'عمان',
+Admin::Account.create! full_name: 'ابن سينا', username: 'admin', password: 'admin', deletable: true
+
+job_requests = [
+  {full_name: 'الخليل أحمد الفراهيدي', phone: '0512345678', address: 'عمان',
     specialization: 'علم النحو و البلاغة', degree: 'عالم مسلم'},
-  {first_name: 'ابن', last_name: 'خلدون', phone: '0521345678', address: 'تونس',
+  {full_name: 'ابن خلدون', phone: '0521345678', address: 'تونس',
     specialization: 'علم الاجتماع', degree: 'عالم مسلم'},
-  {first_name: 'ابن', last_name: 'سينا', phone: '0521345678', address: 'بخارى',
+  {full_name: 'ابن سينا', phone: '0521345678', address: 'بخارى',
     specialization: 'رياضيات', degree: 'عالم مسلم'},
 ]
 
-applicants.each do |applicant|
-  Applicant::JobRequest.create! applicant
+job_requests.each do |job_request|
+  Applicant::JobRequest.create! job_request
 end
