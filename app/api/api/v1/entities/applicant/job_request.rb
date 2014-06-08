6ebecc0ad8dd277834e907731b3583431101217b
Abdullah Alansari <ahimta@gmail.com>
2014-06-08 05:22:34 +0300

    fix applicant_job_request entity name

--- app/api/api/v1/entities/applicant/job_request.rb
+++ app/api/api/v1/entities/applicant/job_request.rb
@@ -1,5 +1,5 @@
 class API::V1::Entities::Applicant::JobRequest < API::V1::Entities::Base
-  root :applicant_job_requests, :applicant_job_requests
+  root :applicant_job_requests, :applicant_job_request
 
   expose :full_name, :phone, :address, :specialization, :degree
 
