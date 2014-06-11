177a3533e1bf032e09ce46f7e4471e10760c9c65
Abdullah Alansari <ahimta@gmail.com>
2014-06-11 08:02:19 +0300

    change logout shared spec to test accoutnable/logout

--- spec/support/shared_specs/controllers/accountable/logout.rb
+++ spec/support/shared_specs/controllers/accountable/logout.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 shared_examples 'controllers/accountable/logout' do |model, resource|
-  name = resource.split('/').join('_')[0..-2]
 
-  describe "DELETE /api/v1/#{resource}/logout" do
+  name = resource.split('/').join('_')[0..-2]
+  url  = '/api/v1/accountable/logout'
+
+  describe "DELETE #{url}" do
     let!(:user) { FactoryGirl.create name }
     let!(:count) { model.count }
 
-    let(:action!) { delete "/api/v1/#{resource}/logout" }
+    let(:action!) { delete url }
 
     before { expect(model.count).to eq(count) }
     after { expect(model.count).to eq(count) }
