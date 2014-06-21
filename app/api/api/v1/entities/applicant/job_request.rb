94ab219306a9ea8d2eac93d1020f1e37dc4073d9
Abdullah Alansari <ahimta@gmail.com>
2014-06-21 13:17:06 +0300

    update applicant_job_requests entities to include files' urls

--- app/api/api/v1/entities/applicant/job_request.rb
+++ app/api/api/v1/entities/applicant/job_request.rb
@@ -5,4 +5,17 @@ class API::V1::Entities::Applicant::JobRequest < API::V1::Entities::Base
   expose :accepted do |applicant, _|
     applicant.accepted_status
   end
+
+  expose :bachelor_certificate do |applicant, _|
+    applicant.bachelor_certificate.url
+  end
+  expose :master_certificate do |applicant, _|
+    applicant.master_certificate.url
+  end
+  expose :bachelor_certificate do |applicant, _|
+    applicant.doctorate_certificate.url
+  end
+  expose :bachelor_certificate do |applicant, _|
+    applicant.cv.url
+  end
 end
