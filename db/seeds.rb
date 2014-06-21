11071934e9c328afca0bfb2b86ca4bb75ccf5695
Abdullah Alansari <ahimta@gmail.com>
2014-06-21 13:11:53 +0300

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ Admin::Account.create! full_name: 'ابن سينا', username: 'admin', password
 
 job_requests = [
   {full_name: 'الخليل أحمد الفراهيدي', phone: '0512345678', address: 'عمان',
-    specialization: 'علم النحو و البلاغة', degree: 'عالم مسلم'},
+    specialization: 'علوم شرعية', degree: 'أستاذ'},
   {full_name: 'ابن خلدون', phone: '0521345678', address: 'تونس',
-    specialization: 'علم الاجتماع', degree: 'عالم مسلم'},
+    specialization: 'فيزياء', degree: 'أستذ'},
   {full_name: 'ابن سينا', phone: '0521345678', address: 'بخارى',
-    specialization: 'رياضيات', degree: 'عالم مسلم'},
+    specialization: 'كيمياء', degree: 'أستاذ'},
 ]
 
 job_requests.each do |job_request|
